4e88136448affc11fbb330a89d845b168c7db9c6
moskvin_oleksandr <moskvin_oleksandr@proton.me>
2026-06-05 22:09:53 +0300

    start task, Implement number reverter

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -2,7 +2,12 @@ package com.epam.langSyntax;
 
 public class NumberReverter {
 
+    //897
     public void revert(int number) {
+        var value = String.valueOf(number);
+        var reversed =  new StringBuilder(value).reverse().toString();
+
+        System.out.println(reversed);
     }
 
 }
